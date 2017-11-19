356fd855e01289a65444a9d351a76277a00642ce
Dethbug <mccjul@gmail.com>
2017-11-18 20:08:48 -0500

    Get map box and do live updates of graphs on scroll and movement

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import dash_html_components as html
 import plotly.plotly as py
 from plotly import graph_objs as go
 from plotly.graph_objs import *
+import util
 
 app = dash.Dash()
 
@@ -20,24 +21,19 @@ app.layout = html.Div(children=[
                     figure={
                         'data': [
                             Scattermapbox(
-                                lat=['40.7272'],
-                                lon=['-73.5673'],
+                                lat=['40.7272', '40.6945', '40.7272'],
+                                lon=['-73.991251', '-73.991251', '-73.911200'],
                                 mode='markers',
                                 marker=Marker(
                                     size=14
                                 ),
-                                text=['Montreal'],
+                                text=['Montreal', 'Bob town', 'Mary town'],
                             )
                         ],
                         'layout': go.Layout(
                             autosize=True,
                             height=500,
-                            margin=dict(
-                                l=0,
-                                r=0,
-                                b=0,
-                                t=0
-                            ),
+                            margin=dict(l=0, r=0, b=0, t=0),
                             showlegend=False,
                             mapbox=dict(
                                 accesstoken=mapbox_access_token,
@@ -100,7 +96,7 @@ app.layout = html.Div(children=[
             figure={
                 'data': [
                     {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'Montréal'},
                 ],
                 'layout': {
                     'title': 'Chart 3',
@@ -109,12 +105,66 @@ app.layout = html.Div(children=[
                         l=0,
                         r=0,
                         b=0,
-                        t=40)
+                        t=100)
                 }
             }
         )
     ])
 ])
+
+@app.callback(Output('example-graph1', 'figure'), [], [State('map-graph', 'relayoutData')],
+              [Event('map-graph', 'relayout')])
+def updateGraph1(relayoutData):
+    res = boundBox(relayoutData)
+    print(res)
+    return {
+        'data': [
+            {'x': [1, 2, 3], 'y': [2, 1, 2], 'type': 'bar', 'name': 'SF'},
+        ],
+        'layout': {
+            'title': 'Chart 1',
+            'height': '300',
+            'margin': dict(l=0, r=0, b=0, t=100)
+        }
+    }
+
+@app.callback(Output('example-graph2', 'figure'), [], [State('map-graph', 'relayoutData')],
+              [Event('map-graph', 'relayout')])
+def updateGraph2(relayoutData):
+    res=boundBox(relayoutData)
+    print(res)
+    return {
+        'data': [
+            {'x': [1, 2, 3], 'y': [2, 1, 2], 'type': 'bar', 'name': 'SF'},
+        ],
+        'layout': {
+            'title': 'Chart 2',
+            'height': '300',
+            'margin': dict(l=0, r=0, b=0, t=100)
+        }
+    }
+
+@app.callback(Output('example-graph3', 'figure'), [], [State('map-graph', 'relayoutData')],
+              [Event('map-graph', 'relayout')])
+def updateGraph3(relayoutData):
+    res = boundBox(relayoutData)
+    print(res)
+    return {
+        'data': [
+            {'x': [1, 2, 3], 'y': [2, 1, 2], 'type': 'bar', 'name': 'SF'},
+        ],
+        'layout': {
+            'title': 'Chart 3',
+            'height': '300',
+            'margin': dict(l=0, r=0, b=0, t=100)
+        }
+    }
+
+def boundBox(relayout):
+    lng = float(relayout['mapbox']['center']['lon'])
+    lat = float(relayout['mapbox']['center']['lat'])
+    zoom = float(relayout['mapbox']['zoom'])
+    return util.getCorners({'lat': lat, 'lng': lng}, zoom,  400, 500)
 
 app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
 
