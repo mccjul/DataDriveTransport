09357efccf45e16687d407753696c578f26b6a07
ALL-MIGHTY-LABT\karel <mirakl834@gmail.com>
2017-11-18 19:40:42 -0500

    uncommented the changes

--- dataparser.py
+++ dataparser.py
@@ -9,22 +9,22 @@ import numpy as np
 df = pd.read_csv('data/Stations.csv')
 bikepostList = [None]*3005
 
-# for index, row in bf.iterrows():
-#     bikepostList[row['Station ID']] = bikepost.BikePost(row['Station Name'], row['Latitude'], row['Longitude'])
-#
-# # Test
-# print(len(bikepostList))
-# print(bikepostList[3002])
-#
-# # Parse trips
-#
-# #Initialize Variables
-# totalNumberTrips = 0
-# totalDuration = 0
-#
-# # Create trip object
-# totalNumberTrips += 1
-# totalDuration += 0 # Certain data
+for index, row in bf.iterrows():
+    bikepostList[row['Station ID']] = bikepost.BikePost(row['Station Name'], row['Latitude'], row['Longitude'])
+
+# Test
+print(len(bikepostList))
+print(bikepostList[3002])
+
+# Parse trips
+
+#Initialize Variables
+totalNumberTrips = 0
+totalDuration = 0
+
+# Create trip object
+totalNumberTrips += 1
+totalDuration += 0 # Certain data
 
 
 
